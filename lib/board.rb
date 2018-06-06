1194928b0cce80e45aa99ed63f6004f7bb511d0e
Albert Zhao <albertzhao629@gmail.com>
2018-06-06 23:52:34 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,12 @@
 # Define the variable board below.
+board = []
+board = "asas "
+board = "eaea "
+board = "eef "
+board = "edas "
+board = "efrg "
+board = "waef "
+board = "rgww "
+board = "qwef "
+board = "efwq "
+
